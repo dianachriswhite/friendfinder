0504a1a9239654a1e12bbbb1abe11ed57c53fbd4
Diana <diana@Dianas-MacBook-Air.local>
2018-01-16 23:10:33 -0800

    first commit

---
+++ data/friends.js
@@ -0,0 +1,22 @@
+
+var friends =[{
+  "name":"Dog",
+  
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}];
+
+
+
+
+module.exports = friends;
